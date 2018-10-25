77f80b0c20ee62d7c6a21c036e2b88ccc5de5a9b
mferran <mferran@eecs.wsu.edu>
2018-10-25 15:56:47 -0700

    Oh. It's seed for this one...

--- Project4/pa4.c
+++ Project4/pa4.c
@@ -4,6 +4,7 @@
 int *serial_baseline(int n, int A, int B, int P, int output[]){
 	int seed = 909;
 	int i = 0;
+	output[0] = seed;
 	printf("serial[0] : %d \n", seed);
 	for(i = 1; i<n; i++){
 		output[i] = (A* (output[i-1] ) + B )% P;
@@ -13,18 +14,19 @@ int *serial_baseline(int n, int A, int B, int P, int output[]){
 	return output;
 }
 
+
 int *serial_matrix(int n, int A, int B, int P, int output[]){
 	int seed = 909;
 	int i = 0;
-	int tA = A, tB = B;
-	int matrix[2][2] = { {A, 0},
-						 {B, 1} };
+	long long int tA = A, tB = B;
+	output[0] = seed;
 	printf("matrix[0] : %d \n", seed);
 	for(i = 1; i<n; i++){
 		//calculate our matrix to the i-th power
-		output[i] = (tA* (output[i-1] ) +  1 * tB )% P;
+		output[i] = (tA* seed + tB )% P;
+		printf("[%lld, 0]\n[%lld, 1]\n\n", tA,tB);
 		printf("matrix[%d] : %d \n", i, output[i]);
-		tA = tA*A;
+		tA = tA*A + 0;
 		tB = tB*A + B;
 	}
 
